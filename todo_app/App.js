7303d1ba78c5cc3aa49a5b15e1a5907dc3c0ec9b
JasminF <60253960+40412@users.noreply.github.com>
2024-12-05 15:12:30 +0200

    created project

---
+++ todo_app/App.js
@@ -0,0 +1,47 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
+import { Login } from "./screens/login/login";
+import { Signup } from "./screens/signup/signup";
+import { Todos } from "./screens/todos/todoScreen";
+
+const Stack = createNativeStackNavigator();
+
+const LoginStack = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="Signup" component={Signup} />
+    </Stack.Navigator>
+  );
+};
+
+const MainContent = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Todos" component={Todos} />
+    </Stack.Navigator>
+  );
+};
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      <Login />
+      {/* <NavigationContainer>
+        <MainContent />
+        <LoginStack />
+      </NavigationContainer> */}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
---
+++ todo_app/screens/todos/todoScreen.js
@@ -0,0 +1,9 @@
+import { Text } from "react-native";
+
+export const Todos = () => {
+  return (
+    <>
+      <Text>Todos</Text>
+    </>
+  );
+};
